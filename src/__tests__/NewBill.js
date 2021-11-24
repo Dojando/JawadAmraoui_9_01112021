024f6ee6bf2c6b88857af5c2d5abf7f7836cf781
jawad <wadja.b@hotmail.fr>
2021-11-24 18:38:23 +0100

    couverture des statements avec les tests et realisation d'un test d'integration POST pour NewBill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -8,14 +8,6 @@ import firebase from "../__mocks__/firebase"
 
 
 describe("Given I am connected as an employee", () => {
-  // describe("When I am on NewBill Page and I click on submit button with incorrect form", () => {
-  //   test("Then It should renders NewBill page", () => {
-  //     const html = NewBillUI()
-  //     document.body.innerHTML = html
-  //     //to-do write assertion
-  //   })
-  // })
-
   describe("When I am on NewBill Page and I click on submit button with correct form", () => {
     test("Then It should renders Bills page", async () => {
       const onNavigate = (pathname) => {
@@ -28,47 +20,24 @@ describe("Given I am connected as an employee", () => {
         type: 'Employee'
       }))
 
+      const html = NewBillUI()
+      document.body.innerHTML = html
+      
       const newBill = new NewBill({
         document, onNavigate, firestore: null, localStorage
       }) 
-
-      const bills = await firebase.get()
-      const html = NewBillUI()
-      document.body.innerHTML = html
-
-      const inputDate = screen.getByTestId("datepicker")
-      fireEvent.change(inputDate, { target: { value: bills.data[0].date } })
-      expect(inputDate.value).toBe(bills.data[0].date)
-
-      const inputAmount = screen.getByTestId("amount")
-      fireEvent.change(inputAmount, { target: { value: bills.data[0].amount } })
-      expect(inputAmount.value).toBe(bills.data[0].amount)
-
-      const inputPct = screen.getByTestId("pct")
-      fireEvent.change(inputPct, { target: { value: bills.data[0].pct } })
-      expect(inputPct.value).toBe(bills.data[0].pct)
-
-      const inputFile = screen.getByTestId("file")
-      fireEvent.change(inputFile, { target: { value: bills.data[0].fileUrl } })
-      expect(inputFile.value).toBe(bills.data[0].fileUrl)
-
+            
+      const handleSubmit = jest.fn((e) => newBill.handleSubmit(e))
       
-      const handleSubmit = jest.fn((e) => newBill.handleSubmit(e)) 
-      
-      const form = screen.getByTestId("form-new-bill")
-      form.addEventListener('submit', handleSubmit)
-      fireEvent.submit(form) 
+      const formButton = screen.getByTestId("form-new-bill")
+      formButton.addEventListener('submit', handleSubmit)
+      fireEvent.submit(formButton) 
+
       expect(handleSubmit).toHaveBeenCalled()
+      const message = await screen.getByText(/Mes notes de frais/)
+      expect(message).toBeTruthy()
     })
   })
-
-  // describe("When I am on NewBill Page and I change the file in the form with an invalid file", () => {
-  //   test("Then the file input should be empty", () => {
-  //     const html = NewBillUI()
-  //     document.body.innerHTML = html
-  //     //to-do write assertion
-  //   })
-  // })
 
   describe("When I am on NewBill Page and I change the file in the form with a valid file", () => {
     test("Then the file should have changed", async () => {
@@ -82,33 +51,71 @@ describe("Given I am connected as an employee", () => {
         type: 'Employee'
       }))
 
-      const newBill = new NewBill({
-        document, onNavigate, firestore: null, localStorage
-      }) 
-
-      
       const html = NewBillUI()
       document.body.innerHTML = html
       const bills = await firebase.get()
 
+      const newBill = new NewBill({
+        document, onNavigate, firestore: null, localStorage
+      }) 
 
       const inputFile = screen.getByTestId('file')
       fireEvent.change(inputFile, { target: { files: [new File(["myProof.png"], "myProof.png", { type: "image/png" })] } })
-      expect(inputFile.value).toBe("myProof.png")
+      expect(inputFile.files[0].name).toBe("myProof.png")
 
-      const handleChangeFile = jest.fn(newBill.handleChangeFile(e)) 
+      const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e)) 
       inputFile.addEventListener('change', handleChangeFile)
       fireEvent.change(inputFile, { target: { files: [new File(["myProof2.png"], "myProof2.png", { type: "image/png" })] } })
       expect(handleChangeFile).toHaveBeenCalled()
-
     })
   })
 })
 
-// // test d'intégration POST
-// describe("Given I am a user connected as Employee", () => {
-//   describe("When I am on NewBill Page", () => {
-//     test("Then, POST new bill", async () => {})
-//     test("Then, fails to POST new bill with message error", async () => {})
-//   })
-// })
+// test d'intégration POST
+describe("Given I am a user connected as Employee", () => {
+  describe("When I am on NewBill Page", () => {
+    test("Then, POST new bill", async () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+
+      const html = NewBillUI()
+      document.body.innerHTML = html
+      
+      const newBill = new NewBill({
+        document, onNavigate, firestore: null, localStorage
+      })
+
+      const bills = await firebase.get()
+
+      const type = screen.getByTestId("expense-type");
+      const nom = screen.getByTestId("expense-name");
+      nom.value = bills.data[1].name;
+      const date = screen.getByTestId("datepicker");
+      date.value = bills.data[1].date;
+      const montant = screen.getByTestId("amount");
+      montant.value = bills.data[1].amount;
+      const pct = screen.getByTestId("pct");
+      pct.value = bills.data[1].pct;
+      const file = screen.getByTestId("file");
+      fireEvent.change(file, { target: { files: [new File(["1592770761.jpeg"], "1592770761.jpeg", { type: "image/png" })] } })
+    
+      const formButton = screen.getByTestId("form-new-bill")
+      formButton.addEventListener('submit', function() {
+        expect(type.value).toBe(bills.data[1].type)
+        expect(nom.value).toBe(bills.data[1].name)
+        expect(date.value).toBe(bills.data[1].date)
+        expect(montant.value).toBe(`${bills.data[1].amount}`)
+        expect(pct.value).toBe(`${bills.data[1].pct}`)
+        expect(file.files[0].name).toBe(bills.data[1].fileName)
+      })
+      fireEvent.submit(formButton) 
+    })
+  })
+})
